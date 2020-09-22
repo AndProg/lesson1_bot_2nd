620f59208b82aaafa954b1cfcc8c51a20283bb79
Андрей <andrej@MacBook-Pro-Andrej.local>
2020-09-22 15:53:43 +0300

    обновил бота

--- lesson1_bot.py
+++ lesson1_bot.py
@@ -1,16 +1,41 @@
-import logging
+import logging, ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
 
-logging.basicConfig(filename='lesson1_bot.log', level=logging.INFO)
+logging.basicConfig(
+    filename='lesson1_bot.log', 
+    level=logging.INFO
+)
 
 PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs':{'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+    'urllib3_proxy_kwargs':{
+        'username': settings.PROXY_USERNAME, 
+        'password': settings.PROXY_PASSWORD
+    }
+}
 
 def greet_user(update, context):
     print('вызван /start')
     update.message.reply_text('Здравствуй, Пользователь!')
+
+
+def stellar(update, context):
+    print('Вызван /planet')
+    update.message.reply_text('Введите название интересующей Вас планеты (на английском) после команды /planet')
+    chosen_planet = str(update.message.text).lower().split()
+    print(chosen_planet)
+    
+    for planet in planet_list:
+      if planet in chosen_planet:
+
+        planet = planet.capitalize()
+        planet_coords = getattr(ephem, planet)(ephem.now())
+        constel = ephem.constellation(planet_coords)
+        
+        update.message.reply_text('Планета находится в созвездии {}'.format(constel))
+        break
+ 
 
 def talk_to_me(update, context):
     text = update.message.text
@@ -23,6 +48,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', stellar))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
@@ -30,5 +56,6 @@ def main():
     mybot.idle()
 
 if __name__ == '__main__':
+    planet_list = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
     main()
 
